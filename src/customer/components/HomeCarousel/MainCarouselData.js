dd454e5148e9fcc9a7863641cd4d6dfcba92e61a
habib-SE <hab67603@gmail.com>
2024-03-03 15:50:48 +0500

    create ecommerce application

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,6 @@
+// ImageFile.js
+import image1 from '../../assets/beautiful-fashion-woman-in-black-dress-stylish-girl-from-multicolored-paints-splash-of-watercolor-colored-drawing-realistic-illustration-of-paints-vector.jpg';
+import image2 from '../../assets/png-clipart-dress-red-gown-clothing-woman-dress-fashion-girl-thumbnail.png';
+import image3 from '../../assets/png-transparent-fashion-girl-long-haired-girl-drawing-girls-pink-dress-thumbnail.png';
+
+export const images = [image1, image2, image3];
